d45f1d8681246a93cfd512fbc28bceb44e7e7d37
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-16 11:17:32 -0300

    adicionando verificação de erro

--- src/main/java/com/luminary/apiedenmongo/Models/Exception/HttpError.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Exception/HttpError.java
@@ -1,2 +1,12 @@
-package com.luminary.apiedenmongo.Models.Exception;public class HttpError {
+package com.luminary.apiedenmongo.Models.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class HttpError extends RuntimeException {
+    private final HttpStatus httpStatus;
+    private final String message;
 }
