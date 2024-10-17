e3582f8df7194a619f388f5d3ace4d4b5acf3f0f
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-17 10:44:07 -0300

    Renomeando os imports nos repositories

--- src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
+++ src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
@@ -1,6 +1,6 @@
 package com.luminary.apiedenmongo.Repositories;
 
-import com.luminary.apiedenmongo.Models.Forum;
+import com.luminary.apiedenmongo.Models.Database.Forum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
