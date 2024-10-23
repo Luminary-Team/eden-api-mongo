1fbcf6bcead0ff7fef7190974c4018dda5e23074
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-23 11:17:56 -0300

    Removing useless import

--- src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
+++ src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
@@ -5,8 +5,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ForumRepository extends MongoRepository<Forum, ObjectId> {
 }
