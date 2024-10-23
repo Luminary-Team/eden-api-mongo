b1ba2a5e2880c0f52038f100eae75df2a0fa7e51
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-23 09:52:22 -0300

    Add Response method in News and Forum

--- src/main/java/com/luminary/apiedenmongo/Models/Response/NewsResponse.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Response/NewsResponse.java
@@ -1,2 +1,24 @@
-package com.luminary.apiedenmongo.Models.Response;public class NewsResponse {
+package com.luminary.apiedenmongo.Models.Response;
+
+import com.luminary.apiedenmongo.Models.Database.News;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class NewsResponse {
+    private String id;
+    private String url;
+    private String title;
+    private String description;
+    private String date;
+
+    public NewsResponse(News news) {
+        this.id = news.getId().toHexString();
+        this.url = news.getUrl();
+        this.title = news.getTitle();
+        this.description = news.getDescription();
+        this.date = news.getDate();
+    }
 }
