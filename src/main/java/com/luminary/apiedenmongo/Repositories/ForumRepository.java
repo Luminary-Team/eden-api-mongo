3b0d480367ef4a6a55b0373105c63d9a0dfbb477
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-23 09:53:21 -0300

    Updating the Repositories for ObjectId on Id

--- src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
+++ src/main/java/com/luminary/apiedenmongo/Repositories/ForumRepository.java
@@ -1,9 +1,12 @@
 package com.luminary.apiedenmongo.Repositories;
 
 import com.luminary.apiedenmongo.Models.Database.Forum;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ForumRepository extends MongoRepository<Forum, String> {
+public interface ForumRepository extends MongoRepository<Forum, ObjectId> {
 }
