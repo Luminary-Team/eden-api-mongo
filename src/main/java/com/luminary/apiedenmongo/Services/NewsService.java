9e983f5d210024da8bdbd46b08e0b2714ed4d625
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-23 11:14:34 -0300

    Update by putting the logic of getById

--- src/main/java/com/luminary/apiedenmongo/Services/NewsService.java
+++ src/main/java/com/luminary/apiedenmongo/Services/NewsService.java
@@ -1,12 +1,15 @@
 package com.luminary.apiedenmongo.Services;
 
-import com.luminary.apiedenmongo.Repositories.NewsRepository;
 import com.luminary.apiedenmongo.Models.Database.News;
+import com.luminary.apiedenmongo.Models.Response.NewsResponse;
+import com.luminary.apiedenmongo.Repositories.NewsRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,14 +26,19 @@ public class NewsService {
         return newsRepository.findAll();
     }
 
-    public Optional<News> getNewsById(String id) {
+    public ResponseEntity<NewsResponse> getNewsById(String id) {
         log.info("[NEWS] Fetching news by ID: {}", id);
         if (ObjectId.isValid(id)) {
-            log.info("[NEWS] Valid ObjectId: " + id);
-            return newsRepository.findById(new ObjectId(id));
+            log.info("[NEWS] Valid ObjectId: {}", id);
+            Optional<News> newsOptional = newsRepository.findById(new ObjectId(id));
+            if (newsOptional.isPresent()) {
+                NewsResponse response = new NewsResponse(newsOptional.get());
+                return ResponseEntity.ok(response);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
         } else {
-            log.warn("[NEWS] Invalid ObjectId: " + id);
-            return Optional.empty();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
