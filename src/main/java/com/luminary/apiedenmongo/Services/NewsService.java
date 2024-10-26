8ffed3bbef4246729d78028a82313fd2110addc7
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-10-25 23:04:15 -0300

    updating return and deleting findById in service

--- src/main/java/com/luminary/apiedenmongo/Services/NewsService.java
+++ src/main/java/com/luminary/apiedenmongo/Services/NewsService.java
@@ -1,23 +1,15 @@
 package com.luminary.apiedenmongo.Services;
 
-import com.luminary.apiedenmongo.Models.Database.News;
-import com.luminary.apiedenmongo.Models.Response.ForumResponse;
 import com.luminary.apiedenmongo.Models.Response.NewsResponse;
 import com.luminary.apiedenmongo.Repositories.NewsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.ObjectId;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 @Slf4j
 public class NewsService {
     private final NewsRepository newsRepository;
@@ -27,21 +19,5 @@ public class NewsService {
         return newsRepository.findAll().stream()
                 .map(NewsResponse::new)
                 .toList();
-    }
-
-    public ResponseEntity<NewsResponse> getNewsById(String id) {
-        log.info("[NEWS] Fetching news by ID: {}", id);
-        if (ObjectId.isValid(id)) {
-            log.info("[NEWS] Valid ObjectId: {}", id);
-            Optional<News> newsOptional = newsRepository.findById(new ObjectId(id));
-            if (newsOptional.isPresent()) {
-                NewsResponse response = new NewsResponse(newsOptional.get());
-                return ResponseEntity.ok(response);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
     }
 }
