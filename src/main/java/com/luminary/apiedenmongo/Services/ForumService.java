0e65e2a8104f288345f9f46330406f1541c6d4ac
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-02 23:55:27 -0300

    Update ForumService.java

--- src/main/java/com/luminary/apiedenmongo/Services/ForumService.java
+++ src/main/java/com/luminary/apiedenmongo/Services/ForumService.java
@@ -1,58 +1,69 @@
-package com.luminary.apiedenmongo.Models.Database;
+package com.luminary.apiedenmongo.Services;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.luminary.apiedenmongo.Models.Database.Forum;
+import com.luminary.apiedenmongo.Models.Request.ForumRequest;
+import com.luminary.apiedenmongo.Models.Response.ForumResponse;
+import com.luminary.apiedenmongo.Repositories.ForumRepository;
+import com.luminary.apiedenmongo.Models.Exception.HttpError;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@ToString
-@Document(collection = "Forum")
-@Schema(description = "Representation of a forum with content, comments and likes.")
-public class Forum {
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ForumService {
+    private final ForumRepository forumRepository;
 
-    @Id
-    @Field(name = "_id")
-    private ObjectId id;
+    public List<ForumResponse> getAllForums() {
+        log.info("[FORUM] Fetching all forums");
+        return forumRepository.findAll().stream()
+                .map(ForumResponse::new)
+                .toList();
+    }
 
-    @Field(name = "user_id")
-    @NotNull(message = "user_id cannot be null")
-    @Schema(description = "User ID of the forum creator.", example = "12345")
-    private int userId;
+    public ForumResponse getForumById(String id) {
+        log.info("[FORUM] Fetching forum by ID: " + id);
+        Forum forum = forumRepository.findById(new ObjectId(id))
+                .orElseThrow(() -> new HttpError(HttpStatus.BAD_REQUEST, "Fórum não encontrado"));
+        log.info("[FORUM] Forum found with ID: " + id);
+        return new ForumResponse(forum);
+    }
 
-    @Field(name = "content")
-    @NotNull(message = "content cannot be null")
-    @Schema(description = "Forum content.", example = "This is the forum content")
-    private String content;
+    public ForumResponse createForum(ForumRequest forumRequest) {
+        log.info("[FORUM] Creating forum: " + forumRequest);
 
-    @Field(name = "comments")
-    private List<Comment> comments;
+        Forum forum = new Forum();
+        forum.setUserId(forumRequest.getUserId());
+        forum.setContent(forumRequest.getContent());
+        forum.setComments(new ArrayList<>());
 
-    @Field(name = "post_date")
-    private LocalDateTime postDate = LocalDateTime.now();
+        log.info("[FORUM] Persisting forum in database");
+        Forum savedForum = forumRepository.save(forum);
+        log.info("[FORUM] Forum created successfully: " + savedForum);
 
-    @Getter
-    @Setter
-    @ToString
-    @Schema(description = "Comment made by a user in the forum.")
-    public static class Comment {
-        @Schema(description = "User ID of the comment.", example = "123")
-        @NotNull(message = "user_id cannot be null")
-        private int userId;
+        return new ForumResponse(savedForum);
+    }
 
-        @Schema(description = "Content of the comment.", example = "This is a content.")
-        @NotNull(message = "content cannot be null")
-        private String content;
+    public ForumResponse addComment(String forumId, Forum.Comment comment) {
+        log.info("[FORUM] Adding comment to forum ID: " + forumId);
+        Forum forum = forumRepository.findById(new ObjectId(forumId))
+                .orElseThrow(() -> new HttpError(HttpStatus.BAD_REQUEST, "Fórum não encontrado"));
 
-        private LocalDateTime postDate = LocalDateTime.now();
+        if (forum.getComments() == null) {
+            forum.setComments(new ArrayList<>());
+        }
+
+        forum.getComments().add(comment);
+        log.info("[FORUM] Persisting updated forum in database");
+        Forum updatedForum = forumRepository.save(forum);
+        log.info("[FORUM] Comment added successfully to forum ID: " + forumId);
+
+        return new ForumResponse(updatedForum);
     }
 }
