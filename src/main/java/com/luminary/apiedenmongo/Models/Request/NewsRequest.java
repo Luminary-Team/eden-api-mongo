056f23d1934fe1c4b6af05422a643da87a4e0922
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-02 23:54:05 -0300

    Update NewsRequest.java

--- src/main/java/com/luminary/apiedenmongo/Models/Request/NewsRequest.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Request/NewsRequest.java
@@ -3,6 +3,9 @@ package com.luminary.apiedenmongo.Models.Request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 @Setter
 public class NewsRequest {
@@ -10,12 +13,10 @@ public class NewsRequest {
     private String url;
     private String title;
     private String description;
-    private String date;
 
-    public NewsRequest(String url, String title, String description, String date) {
+    public NewsRequest(String url, String title, String description) {
         this.url = url;
         this.title = title;
         this.description = description;
-        this.date = date;
     }
 }
