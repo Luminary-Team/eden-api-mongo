a115664268ae2f6afdd6acdef2b08b0afdfaf14a
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-02 23:53:29 -0300

    Update ForumRequest.java

--- src/main/java/com/luminary/apiedenmongo/Models/Request/ForumRequest.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Request/ForumRequest.java
@@ -1,10 +1,13 @@
 package com.luminary.apiedenmongo.Models.Request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -12,12 +15,10 @@ public class ForumRequest {
 
     @Schema(description = "User ID of the forum creator.", example = "12345")
     @NotNull(message = "user_id cannot be null")
-    @NotEmpty(message = "user_id cannot be empty")
     private int userId;
 
     @Schema(description = "Forum content.", example = "This is the forum content")
     @NotNull(message = "content cannot be null")
-    @NotEmpty(message = "content cannot be empty")
     private String content;
 
     public ForumRequest(int userId, String content) {
