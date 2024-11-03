86520fd742d50242efbc65a6d153b15f649706a1
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-02 23:52:20 -0300

    Update ForumController.java

--- src/main/java/com/luminary/apiedenmongo/Controllers/ForumController.java
+++ src/main/java/com/luminary/apiedenmongo/Controllers/ForumController.java
@@ -2,7 +2,6 @@ package com.luminary.apiedenmongo.Controllers;
 
 import com.luminary.apiedenmongo.Models.Database.Forum;
 import com.luminary.apiedenmongo.Models.Request.ForumRequest;
-import com.luminary.apiedenmongo.Models.Request.LikeRequest;
 import com.luminary.apiedenmongo.Models.Response.ForumResponse;
 import com.luminary.apiedenmongo.Services.ForumService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -62,15 +62,5 @@ public class ForumController {
     @PostMapping("/comment/{forumId}")
     public ResponseEntity<ForumResponse> addComment(@PathVariable("forumId") String forumId, @RequestBody Forum.Comment comment) {
         return ResponseEntity.status(HttpStatus.OK).body(forumService.addComment(forumId, comment));
-    }
-
-    @Operation(summary = "Add like to forum", description = "Add a new like to a forum.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Like added successfully"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @PostMapping("/like/{forumId}")
-    public ResponseEntity<ForumResponse> addLike(@PathVariable("forumId") String forumId, @RequestBody LikeRequest likeRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(forumService.addLike(forumId, likeRequest));
     }
 }
