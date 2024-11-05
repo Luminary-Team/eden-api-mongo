66d2010f68fcde5ba9a3d46bdc3f61201ba6fdd6
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-05 20:48:21 -0300

    Update ForumResponse.java

--- src/main/java/com/luminary/apiedenmongo/Models/Response/ForumResponse.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Response/ForumResponse.java
@@ -13,14 +13,16 @@ public class ForumResponse {
     private String id;
     private int userId;
     private String content;
-    private List<Forum.Comment> comments;
     private LocalDateTime postDate;
+    private List<Forum.Comment> comments;
+    private List<Integer> engager;
 
     public ForumResponse(Forum forum) {
         this.id = forum.getId().toHexString();
         this.userId = forum.getUserId();
         this.content = forum.getContent();
-        this.comments = forum.getComments();
         this.postDate = forum.getPostDate();
+        this.comments = forum.getComments();
+        this.engager = forum.getEngager();
     }
 }
