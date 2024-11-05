76d7156f1abbff86d5cf6b2ab11745b4bf961c54
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2024-11-05 20:48:32 -0300

    Update NewsResponse.java

--- src/main/java/com/luminary/apiedenmongo/Models/Response/NewsResponse.java
+++ src/main/java/com/luminary/apiedenmongo/Models/Response/NewsResponse.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Getter
@@ -15,11 +16,13 @@ public class NewsResponse {
     private String title;
     private String description;
     private LocalDateTime date;
+    private List<Integer> engager;
 
     public NewsResponse(News news) {
         this.id = news.getId().toHexString();
         this.url = news.getUrl();
         this.title = news.getTitle();
         this.description = news.getDescription();
+        this.engager = news.getEngager();
     }
 }
